424cb66769e0ff6c704cf85425c357c65d0b5236
神楽坂梓月 <OpenGL106@gmail.com>
2020-08-06 15:33:34 +0800

    updated to version 1.0

--- index.js
+++ index.js
@@ -1,139 +1,152 @@
+//Project F. C. F. (Fuck Chinese Followers) first work, version 1.0.0.
+//Copyright LabMikazu (毅航实验室), Shizuki Kagurazaka (Ziyue Ji), 2020.8.6.
+
 console.log("the tool is running");
 
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+  input: process.stdin,
+  output: process.stdout
+});
 
+//function getFollowers
+//arguments: the authentification T, the screenName, time interval, already listed followers, cursor
+//return: a Promise either to be resolve(list) or reject(err), where list is the listed followers.
 function getFollowers (T, screenName, interval = 5 * 62 * 1000, followers = [], cursor = -1) {
-    return new Promise((resolve, reject) => {
-      T.get('followers/ids', { screen_name: screenName, stringify_ids: true, cursor: cursor, count: 5000 }, (err, data, response) => {
-        if (err) {
-          if (err.message === 'Rate limit exceeded') {
-            console.log("Rate limit reached. Wait for 300 seconds. No.1")
-            setTimeout(() => {
-              return resolve(getFollowers(T, screenName, interval, followers, cursor))
-            }, interval)
-          } else {
-            cursor = -1
-            reject(err)
-          }
-        } else {
-          cursor = data.next_cursor_str
-          followers.push(data.ids)
-          if (cursor != '0') {
-            return resolve(getFollowers(T, screenName, interval, followers, cursor))
-          } else {
-            return resolve([].concat(...followers))
-          }
-        }
-      })
-    })
-}
-
-function ismyfriend(singleid, T){
-    return new Promise((resolve, reject) => {
-        T.get('friendships/lookup', { user_id: singleid },  function (err, data, response) {
-          if (err) {
-            if (err.message === 'Rate limit exceeded') {
-              console.log("Rate limit reached. Wait for 300 seconds. No.2")
-              return resolve("RATE LIMIT EXCEEDED");
-            } else {
-              reject("ERROR!")
-            }
-          } else {
-            if (data[0].connections.includes('following')) {
-                return resolve(1);
-            } else {
-                return resolve(0);
-            }
-          }
-        });
-    })
-}
-
-function singleidprocess(singleid, index, friend, T, blocklist){
   return new Promise((resolve, reject) => {
-    console.log("index %d, processing id: %s", index, singleid);
-    T.get('users/show', { user_id: singleid },  function (err, data, response) {
-      if(err){
+    T.get('followers/ids', { screen_name: screenName, stringify_ids: true, cursor: cursor, count: 5000 }, (err, data, response) => {
+      if (err) {
         if (err.message === 'Rate limit exceeded') {
-          console.log("Rate limit reached. Wait for 300 seconds. No.3")
-          return resolve("RATE LIMIT EXCEEDED")
+          console.log("Rate limit reached. Wait for 300 seconds. No.1")
+          setTimeout(() => {
+            return resolve(getFollowers(T, screenName, interval, followers, cursor))
+          }, interval)
         } else {
-          reject("ERROR!")
+          cursor = -1
+          reject(err)
         }
-      }else {
-        var name = data.screen_name;
-        console.log("user screen name: %s", name);
-        if (friend == 1) {
-            console.log("This user is my friend. Move on to the next one.")
-            return resolve(1);
-        } else if (data.followers_count >= 100) {
-            console.log("This user is not my friend, but appear to not be a Chinese bot. Move on to the next one.")
-            return resolve(1);
-        } else if (data.followers_count <= 5) {
-            console.log("This user is definitely a Chinese bot. Listed!")
-            blocklist.push(name);
-            return resolve(1);
+      } else {
+        cursor = data.next_cursor_str
+        followers.push(data.ids)
+        if (cursor != '0') {
+          return resolve(getFollowers(T, screenName, interval, followers, cursor))
         } else {
-            console.log("This user is not my friend, and it is not sure whether this user is a Chinese bot.")
-            return resolve(1);
+          return resolve([].concat(...followers))
         }
       }
     })
   })
-    
 }
 
-function idlistprocess(idlist, blocklist, T, index){
-    return new Promise((resolve, reject) => {
-      console.log("The number of your follower is: %d", idlist.length);
-      var processinterval = setInterval(() =>{
-        ismyfriend(idlist[index], T).then(function(friend){
-          return new Promise((resolve, reject) =>{
-              if (friend === "RATE LIMIT EXCEEDED"){
-                return resolve("RATE LIMIT EXCEEDED");
-              }else{
-                singleidprocess(idlist[index], index, friend, T, blocklist).then(results => {
-                  if(results){
-                    return resolve(1);
-                  }else{
-                    return reject("ERROR!")
-                  }
-                });
-              }
-          })
-        }).then(result => {
-          console.log("the result of singleidprocess is %d", result);
-          if (result == 1){
-            index += 1;
-            if (index >= idlist.length){
-              clearInterval(processinterval);
-            }
-          } else if (result === "RATE LIMIT EXCEEDED"){
-            console.log("Rate limit reached. Wait for 300 seconds. No.4")
-            clearInterval(processinterval);
-          } else {
-            throw new Error("ERROR!")
-          }
-        })
-      }, 5000);
-      if(index >= idlist.length){
-        return resolve(1);
-      }else {
-        setTimeout(() => {
-          return resolve(idlistprocess(idlist, blocklist, T, index))
-        }, 5 * 62 * 1000)
+//function getFriends
+//arguments: the authentification T, the screenName, time interval, already listed friends, cursor
+//return: a Promise either to be resolve(list) or reject(err), where list is the listed friends.
+function getFriends (T, screenName, interval = 5 * 62 * 1000, friends = [], cursor = -1) {
+  return new Promise((resolve, reject) => {
+    T.get('friends/ids', { screen_name: screenName, stringify_ids: true, cursor: cursor, count: 5000 }, (err, data, response) => {
+      if (err) {
+        if (err.message === 'Rate limit exceeded') {
+          console.log("Rate limit reached. Wait for 300 seconds. No.2")
+          setTimeout(() => {
+            return resolve(getFriends(T, screenName, interval, friends, cursor))
+          }, interval)
+        } else {
+          cursor = -1
+          reject(err)
+        }
+      } else {
+        cursor = data.next_cursor_str
+        friends.push(data.ids)
+        if (cursor != '0') {
+          return resolve(getFriends(T, screenName, interval, friends, cursor))
+        } else {
+          return resolve([].concat(...friends))
+        }
       }
     })
+  })
 }
-function blocklistprocess(blocklist, T, index){
+
+//function listtruefollowers
+//arguments: idlist, friendslist
+//return: a new list contains all that appeared in idlist but not in friendslist
+function listtruefollowers(idlist, friendslist){
+  var newlist = [];
+  idlist.forEach((item) => {
+    if (!(friendslist.includes(item))){
+      newlist.push(item);
+    }
+  });
+  return newlist;
+}
+
+//function judge
+//arguments: a user object
+//return: 1 if judged Chinese bot and 0 if not so.
+function judge(user){
+  if (user.followers_count >= 100){
+    return 0;
+  } else if (user.followers_count <= 5){
+    return 1;
+  } else if (user.followers_count <= 50 && parseFloat(user.followers_count) / parseFloat(user.friends_count) <= 0.05){
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+//function listprocess
+//arguments: an id list, the authentication T, time interval, already listed blockers, cursor
+//return: a Promise either to be reject(err) or resolve(list), where list is the list of blockers.
+function listprocess (list, T, interval = 5 * 62 * 1000, blockers = [], cursor = 0) {
   return new Promise((resolve, reject) => {
-    console.log("Starting to block those on the List.")
-    var blockinterval = setInterval (() => {
-      {
-        T.post('blocks/create', { screen_name: blocklist[index] }, function (err, data, response) {
+    T.get('users/lookup', { user_id: list.slice(cursor, cursor + 100 >= list.length ? list.length : cursor + 100) }, (err, data, response) => {
+      if (err) {
+        if (err.message === 'Rate limit exceeded') {
+          console.log("Rate limit reached. Wait for 300 seconds. No.3")
+          setTimeout(() => {
+            return resolve(listprocess(list, T, interval, blockers, cursor))
+          }, interval)
+        } else {
+          reject(err)
+        }
+      } else {
+        cursor = cursor + 100
+        data.forEach((item) => {
+          if(judge(item)){
+            blockers.push(item.screen_name);
+          }
+        })
+        console.log("Scanned: %d, Detected Chinese bots: %d", cursor >= list.length ? list.length : cursor , blockers.length);
+        if (cursor < list.length) {
+          return resolve(listprocess(list, T, interval, blockers, cursor))
+        } else {
+          return resolve([].concat(...blockers))
+        }
+      }
+    })
+  })
+}
+
+//function blockprocess
+//arguments: a list, the authentication T, time interval, cursor
+//return: a Promise either to be resolve(1) or reject(err).
+function blockprocess(list, T, interval = 5 * 62 * 1000, cursor = 0){
+  return new Promise((resolve, reject) => {
+    var blockinterval = setInterval(() => {
+      T.post('blocks/create', { screen_name: list[cursor] }, function (err, data, response) {
+        if(err){
+          if (err.message === 'Rate limit exceeded') {
+            console.log("Rate limit reached. Wait for 300 seconds. No.4")
+            clearInterval(blockinterval);
+          } else {
+            reject("ERROR!")
+          }
+        }else{
+          console.log("Successfully blocked the %d Chinese bot: %s", cursor + 1, data.screen_name);
+        }
+      });
+      setTimeout(() => {
+        T.post('blocks/destroy', { screen_name: list[cursor] }, function (err, data, response) {
           if(err){
             if (err.message === 'Rate limit exceeded') {
               console.log("Rate limit reached. Wait for 300 seconds. No.5")
@@ -142,45 +155,54 @@ function blocklistprocess(blocklist, T, index){
               reject("ERROR!")
             }
           }else{
-            console.log("Successfully blocked %s", data.screen_name);
-            index += 1;
-            if(index >= blocklist.length){
+            console.log("Successfully unblocked the %d Chinese bot: %s", cursor + 1, data.screen_name);
+            cursor += 1;
+            if(cursor >= list.length){
               clearInterval(blockinterval);
             }
           }
-        })
-      }
-    }, 5000);
-    if (index >= blocklist.length) {
+        });
+      }, 5000);
+    }, 10000);
+    if (cursor >= list.length){
       return resolve(1);
-    }else{
+    } else {
       setTimeout(() => {
-        return resolve(blocklistprocess(blocklist, T, index))
-      }, 5 * 62 * 1000)
+        return resolve(blockprocess(list, T, interval, cursor));
+      }, interval);
     }
   })
 }
 
+
+//function main
+//arguments: scrid
+//return: none
 function main(scrid){
-    var Twit = require('twit');
-    var config = require('./config');
-    var T = new Twit(config);
-    var blocklist = [];
-    getFollowers(T, scrid).then(idlist => {
-        idlistprocess(idlist, blocklist, T, 0).then((results) => {
-          if(results == 1){
-            blocklistprocess(blocklist, T, 0).then((results) => {
-              if(results == 1){
-                console.log("The tool successfully in fucking Chinese followers.")
-              } else {
-                throw new Error("ERROR!")
-              }
-            })
-          } else {
+  var Twit = require('twit');
+  var config = require('./config');
+  var T = new Twit(config);
+  var blocklist = [];
+  getFollowers(T, scrid).then(idlist => {
+    console.log("Your followers number is: %d", idlist.length);
+    getFriends(T, scrid).then(friendslist => {
+      console.log("Your friends number is: %d", friendslist.length);
+      var truelist = listtruefollowers(idlist, friendslist);
+      console.log("Your non-friend followers count: %d", truelist.length);
+      listprocess(truelist, T).then(blocklist => {
+        console.log("The total number of Chinese bots detected: %d", blocklist.length);
+        console.log("This is a list of the first 100 in them.")
+        console.log(blocklist.slice(0, 100));
+        blockprocess(blocklist, T).then(result =>{
+          if(result == 1){
+            console.log("The tool succeeded in fucking Chinese followers.");
+          }else{
             throw new Error("ERROR!")
           }
         })
+      })
     })
+  })
 }
 
 readline.question("Input your screen ID\n", main);
